4d951152b3d47d79c93469ec789d06642e39a58f
Binnukrishna <binnukrishna5233@gmail.com>
2025-01-16 21:46:21 +0530

    lottie removed

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-import LottieLoader from "@/components/LottieLoader";
 
 const Loading = () => {
   return (
     <>
       <main className="w-full h-screen flex flex-col items-center justify-center">
-        <div className="w-40 sm:w-52 h-40 sm:h-52">
-          <LottieLoader />
-        </div>
         <div>
           <h4 className="text-center text-2xl font-bold text-slate-800">
             Sit Tight
