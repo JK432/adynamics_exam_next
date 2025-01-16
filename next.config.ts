bea712d74b9233ba435bc2193b1bc27f65670282
Binnukrishna <binnukrishna5233@gmail.com>
2025-01-16 11:28:05 +0530

    fix try : Next config updated

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.json$/,
-      type: 'json'
-    })
-    return config
-  },
   reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
